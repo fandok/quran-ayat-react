bc0fb4bc659d0bfa122d515758e0d975efc0034d
Fandika Okdiba <mr.parker0@gmail.com>
2019-12-14 20:46:20 +0700

    First commit

---
+++ src/style.js
@@ -0,0 +1,20 @@
+import { css } from '@emotion/core';
+
+export const cssApp = css`
+    text-align: center;
+`;
+
+export const cssAppHeader = css`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+
+    p {
+        width: 80%;
+    }
+`;
